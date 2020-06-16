0a554102525392e571137987d742c34895bd9805
ncampono11 <64989354+ncampono11@users.noreply.github.com>
2020-06-16 19:16:43 -0400

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -24,3 +24,6 @@ with(hpc1, {
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+
+dev.copy(png, file="plot3.png", height=480, width=480)
