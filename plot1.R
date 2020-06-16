acf3d88b20ff10ee54beb1e59688e8aa666ff8cf
ncampono11 <64989354+ncampono11@users.noreply.github.com>
2020-06-16 19:16:01 -0400

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -17,7 +17,7 @@ summary(hpc.1)
 
 hist(hpc1$Global_active_power, col = 'red', main = 'Global Active Power',xlab = 'Global Active Power (Kilowatts)')
 
-
+dev.copy(png, file="plot1.png", height=480, width=480)
 
 
 
